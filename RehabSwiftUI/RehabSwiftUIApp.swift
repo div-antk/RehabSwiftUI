c71e47e67bae6d31e6a70df064be71aa255fef98
div-antk <51283468+owaii@users.noreply.github.com>
2023-03-02 20:33:53 +0900

    Initial Commit

---
+++ RehabSwiftUI/RehabSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RehabSwiftUIApp.swift
+//  RehabSwiftUI
+//
+//  Created by Takuya Ando on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct RehabSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
