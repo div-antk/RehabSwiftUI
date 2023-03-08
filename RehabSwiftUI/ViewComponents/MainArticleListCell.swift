f4272ad4e62d90425c1f634497069c36ef5df9ac
div-antk <51283468+owaii@users.noreply.github.com>
2023-03-08 22:00:16 +0900

    セルのUI修正

--- RehabSwiftUI/ViewComponents/MainArticleListCell.swift
+++ RehabSwiftUI/ViewComponents/MainArticleListCell.swift
@@ -9,32 +9,31 @@ import SwiftUI
 
 struct MainArticleListCell: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Image(systemName: "person")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Name")
-                    .font(.system(size: 16, weight: .bold))
-                Spacer()
+        ZStack {
+            Color
+                .white
+                .cornerRadius(16)
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "person")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Name")
+                        .font(.system(size: 16, weight: .bold))
+                    Spacer()
+                }
+                .padding(.top, 20)
+                .padding(.leading, 16)
+                Text("time")
+                    .padding(.top, 0.4)
+                    .padding(.leading, 16)
+                Text("Title")
+                    .font(.system(size: 20, weight: .bold))
+                    .padding(.top, 4)
+                    .padding(.leading, 16)
+                    .padding(.bottom, 20)
             }
-            .padding(.top, 20)
-            .padding(.leading, 16)
-            Text("time")
-                .padding(.top, 0.4)
-                .padding(.leading, 16)
-            Text("Title")
-                .font(.system(size: 20, weight: .bold))
-                .padding(.top, 4)
-                .padding(.leading, 16)
-                .padding(.bottom, 20)
         }
-        .overlay(
-            RoundedRectangle(cornerRadius: 16)
-                .stroke(
-                    Color.white
-                )
-        )
         .padding(.all, 20)
     }
 }
