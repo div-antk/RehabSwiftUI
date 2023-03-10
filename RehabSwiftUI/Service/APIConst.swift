4af057f528f62ef766f609e3291bcd6c07c6f113
div-antk <51283468+owaii@users.noreply.github.com>
2023-03-10 23:59:00 +0900

    APIConstを作成

---
+++ RehabSwiftUI/Service/APIConst.swift
@@ -0,0 +1,12 @@
+//
+//  APIConst.swift
+//  RehabSwiftUI
+//
+//  Created by Takuya Ando on 2023/03/13.
+//
+
+import Foundation
+
+class APIConst {
+    static let api: String = "https://qiita.com/api/v2/items"
+}
